9b44ac983fd849c20139d9446566957308549a04
Sarathkumar <03sarath@gmail.com>
2025-11-22 14:20:34 +0530

    Pin Sagemaker version to <3 for compatibility
    Updated required packages to pin Sagemaker version to less than 3 to maintain compatibility with pipelines.

--- seed_code/build_app/setup.py
+++ seed_code/build_app/setup.py
@@ -1,35 +1,20 @@
-# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-#
+# Copyright Amazon.com, Inc. or its affiliates.
 # SPDX-License-Identifier: MIT-0
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy of this
-# software and associated documentation files (the "Software"), to deal in the Software
-# without restriction, including without limitation the rights to use, copy, modify,
-# merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
-# permit persons to whom the Software is furnished to do so.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
-# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
-# PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 import os
 import setuptools
-
 
 about = {}
 here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, "ml_pipelines", "__version__.py")) as f:
     exec(f.read(), about)
 
-
 with open("README.md", "r") as f:
     readme = f.read()
 
+# PIN TO SAGEMAKER v2 (v3 breaks pipelines)
+required_packages = ["sagemaker<3"]
 
-required_packages = ["sagemaker"]
 extras = {
     "test": [
         "black",
@@ -39,10 +24,11 @@ extras = {
         "pydocstyle",
         "pytest",
         "pytest-cov",
-        "sagemaker",
+        "sagemaker<3",
         "tox",
     ]
 }
+
 setuptools.setup(
     name=about["__title__"],
     description=about["__description__"],
